3fe35b5ab355e551d781692e165f87ae7ad5f8c2
Nuanpan <pangiiez@gmail.com>
2021-11-18 20:09:00 +0100

    added datacamp.py

---
+++ datacamp.py
@@ -0,0 +1,17 @@
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('/Users/pangiiez/Desktop/prices.csv',index_col=0)
+data = data.iloc[:, [0,1]]
+data.index = pd.to_datetime(data.index)
+data = data.pivot(columns='symbol', values='close')
+data = data[['AAPL','FB','NFLX','V','XOM']]
+print(data.head())
+
+fig, ax = plt.subplots()
+for column in data:
+    data[column].plot(ax=ax, label=column)
+ax.legend()
+plt.show()
+# %%
