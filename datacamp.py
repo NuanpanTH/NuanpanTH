8d82341bee94ac9e153f7503ea13485d400f0a43
Nuanpan <pangiiez@gmail.com>
2021-11-19 16:22:45 +0100

    added EBAY and YHOO prices

--- datacamp.py
+++ datacamp.py
@@ -1,8 +1,7 @@
-#%% Stock Price
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('/Users/pangiiez/Desktop/prices.csv',index_col=0)
+data = pd.read_csv('/Users/pangiiez/workspace/prices.csv',index_col=0)
 data = data.iloc[:, [0,1]]
 data.index = pd.to_datetime(data.index)
 data = data.pivot(columns='symbol', values='close')
@@ -14,4 +13,12 @@ for column in data:
     data[column].plot(ax=ax, label=column)
 ax.legend()
 plt.show()
-# %% 
+
+prices = pd.read_csv('/Users/pangiiez/workspace/prices.csv',index_col=0)
+prices = prices.iloc[:, [0,1]]
+prices.index = pd.to_datetime(prices.index)
+prices = prices.pivot(columns='symbol', values='close')
+prices= prices[['EBAY','YHOO']]
+prices.plot.scatter('EBAY','YHOO',c=prices.index,
+                    cmap=plt.cm.viridis,colorbar=False)
+plt.show()
