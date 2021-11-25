2a29a2bcadba56758b5576b2ad51dc0f8f8aef76
Nuanpan <pangiiez@gmail.com>
2021-11-25 22:36:43 +0100

    added World_Bank

--- datacamp.py
+++ datacamp.py
@@ -18,7 +18,7 @@ prices = pd.read_csv('/Users/pangiiez/workspace/prices.csv',index_col=0)
 prices = prices.iloc[:, [0,1]]
 prices.index = pd.to_datetime(prices.index)
 prices = prices.pivot(columns='symbol', values='close')
-prices= prices[['EBAY','YHOO']]
+prices= prices[['EBAY','YHOO','NVDA','AAPL']]
 prices.plot.scatter('EBAY','YHOO',c=prices.index,
                     cmap=plt.cm.viridis,colorbar=False)
 plt.show()
@@ -55,4 +55,54 @@ print(score)
 fig, ax = plt.subplots(figsize=(15, 5))
 ax.plot(y_test, color='k', lw=3)
 ax.plot(predictions, color='r', lw=2)
+plt.show()
+
+df = pd.read_csv("/Users/pangiiez/workspace/TSLA.csv")
+df['Date'] = pd.to_datetime(df['Date'])
+df['Volume'] = df['Volume'].astype(float)
+df['Gain'] = df['Close'] - df['Open']
+
+# Window Rolling Mean (Moving Average)
+df['Rolling Close Average'] = df['Close'].rolling(2).mean()
+
+df['Rolling Close Average'].plot()
+plt.show()
+
+df['Open Standard Deviation'] = df['Open'].std()
+df['Rolling Open Standard Deviation'] = df['Open'].rolling(2).std()
+print(df[['Date', 'Open', 'Open Standard Deviation', 'Rolling Open Standard Deviation']].head())
+df.plot(x='Date', y='Rolling Open Standard Deviation',legend=False)
+plt.tight_layout()
+plt.show()
+
+df['Rolling Volume Sum'] = df['Volume'].rolling(3).sum()
+print(df[['Date','Volume', 'Rolling Volume Sum']].head())
+
+def percent_change(series):
+    # Collect all *but* the last value of this window, then the final value
+    previous_values = series[:-1] # all but the last value
+    last_value = series[-1] # last value 
+
+    # Calculate the % difference between the last value and the mean of earlier values
+    percent_change = (last_value - np.mean(previous_values)) / np.mean(previous_values)
+    return percent_change
+
+# Apply your custom function and plot
+prices_perc = prices.rolling(20).apply(percent_change)
+
+import numpy as np
+
+# These are the "time lags"
+shifts = np.arange(1, 11).astype(int)
+
+# Use a dictionary comprehension to create name: value pairs, one pair per shift
+shifted_data = {"lag_{}_day".format(day_shift): prices_perc.shift(day_shift) for day_shift in shifts}
+
+# Convert into a DataFrame for subsequent use
+prices_perc_shifted = pd.DataFrame(shifted_data)
+
+# Plot the first 100 samples of each
+ax = prices_perc_shifted.iloc[:100].plot(cmap=plt.cm.viridis)
+prices_perc.iloc[:100].plot(color='r',lw=2)
+ax.legend(loc='best')
 plt.show()
